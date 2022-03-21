d0f93eb0dc30afff5dc55e04733a34a12180d88b
adams100111 <adams100111@users.noreply.github.com>
2022-03-21 12:38:55 +0000

    Fix styling

--- tests/Feature/CheckPermissionTest.php
+++ tests/Feature/CheckPermissionTest.php
@@ -15,4 +15,3 @@ it('can check permission', function () {
     // dd(collect([2,4,6,8,10])->paginate(3, page: 2));
     // expect(Ability::check('sajhfdg'))->toBeTrue();
 })->group('permissions');
-
